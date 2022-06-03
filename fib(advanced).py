89d78aa7a91c8b1fb5b24a120b3a13439c0489df
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2022-06-03 13:56:09 +0530

    startign with dynamic fib

---
+++ fib(advanced).py
@@ -0,0 +1,17 @@
+#Classic Implemenation of fibonnaci series
+
+# def func(n):
+#     if n <= 2:
+#         return 1
+#     else:
+#         return func(n-2) + func(n-1)
+
+
+# print(func(int(input("Input the number: "))))
+
+
+# It have o(2^n) time complexity
+# That really means that if we take fib(50) it goes like: 2^50
+#That is roughly 1125899906842624 steps for the computer
+# so this classic implementation only works for small NUmbers.
+
---
+++ fibonnaciseq.py
@@ -0,0 +1,14 @@
+#Brute Force Method
+
+# def func(n):
+#     arr = [1,1]
+#     a=1
+#     b=1
+#     for i in range(int(n)):
+#         c=a+b
+#         arr.append(c)
+#         a=b
+#         b=c
+#     print(arr[:int(n)])
+
+# func(input("Enter the number: "))
